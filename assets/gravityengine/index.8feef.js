49071b1178f2203cf54403547915b500f199fcaa
MilFun <milfun@qq.com>
2026-05-22 15:07:57 +0800

    first commit

---
+++ assets/gravityengine/index.8feef.js
@@ -0,0 +1,22 @@
+System.register("chunks:///_virtual/gravityengine",["./NewComponent.ts"],(function(){return{setters:[null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/NewComponent.ts",["./rollupPluginModLoBabelHelpers.js","cc"],(function(t){var n,o,e,r;return{setters:[function(t){n=t.inheritsLoose},function(t){o=t.cclegacy,e=t._decorator,r=t.Component}],execute:function(){var c;o._RF.push({},"8440agC1KRJcKELrbImGEop","NewComponent",void 0);var p=e.ccclass;e.property,t("NewComponent",p("NewComponent")(c=function(t){function o(){return t.apply(this,arguments)||this}n(o,t);var e=o.prototype;return e.start=function(){},e.update=function(t){},o}(r))||c);o._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/gravityengine', 'chunks:///_virtual/gravityengine'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
